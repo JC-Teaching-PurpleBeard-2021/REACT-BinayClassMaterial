cdf7ad0a2652e7260953759f2a362b856f069b11
Binay <binaybhusanp@gmail.com>
2021-03-30 09:27:39 +0100

    fixed css import

--- src/modenTodoApp/modernToDoApp/ModernTodo.js
+++ src/modenTodoApp/modernToDoApp/ModernTodo.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import "./App.css";
+import "../../App.css";
 const ModernTodo = () => {
   const [currentTask, setcurrentTask] = useState("");
   const [tasks, settasks] = useState([]);
